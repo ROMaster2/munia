bbc6fc3d777b46f7797eec74408f70b7725414d4
ROMaster2 <ddpoulton+github@gmail.com>
2018-12-14 21:32:22 -0800

    mine

--- MUNIA-win/MUNIA/Controllers/MuniaController.cs
+++ MUNIA-win/MUNIA/Controllers/MuniaController.cs
@@ -1,168 +1,247 @@
-﻿using System;
+﻿using HidSharp;
+using MUNIA.Util;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using HidSharp;
 
-namespace MUNIA.Controllers {
-	public abstract class MuniaController : IController, IDisposable {
-		/// <summary>
-		/// Can be used to register for WM_INPUT messages and parse them.
-		/// For testing purposes it can also be used to solely register for WM_INPUT messages.
-		/// </summary>
+namespace MUNIA.Controllers
+{
+    public abstract class MuniaController : IController, IDisposable
+    {
+        /// <summary>
+        /// Can be used to register for WM_INPUT messages and parse them.
+        /// For testing purposes it can also be used to solely register for WM_INPUT messages.
+        /// </summary>
 
-		public HidDevice HidDevice;
-		private HidStream _stream;
-		public string DevicePath => HidDevice.DevicePath;
-		public virtual string Name => HidDevice.ProductName;
-		public abstract ControllerType Type { get; }
+        public HidDevice HidDevice;
+        private HidStream _stream;
+        public string DevicePath => HidDevice.DevicePath;
+        public virtual string Name => HidDevice.ProductName;
+        public abstract ControllerType Type { get; }
 
-		protected readonly List<bool> _buttons = new List<bool>();
-		protected readonly List<int> _axes = new List<int>();
-		protected readonly List<Hat> _hats = new List<Hat>();
-		public ControllerState GetState() => new ControllerState(_axes, _buttons, _hats);
+        protected readonly List<bool> _buttons = new List<bool>();
+        protected readonly List<int> _axes = new List<int>();
+        protected readonly List<Hat> _hats = new List<Hat>();
+        public ControllerState GetState() => new ControllerState(_axes, _buttons, _hats);
 
-		public event EventHandler StateUpdated;
+        const int LOG_LENGTH = 2097152; // About ten hours worth of inputs at 60Hz, assuming it's being filled the whole time.
+        public List<UltraTime> InputLog = new List<UltraTime>(2097152);
+        public static Stopwatch LogWatch = Stopwatch.StartNew();
 
-		protected MuniaController(HidDevice hidDevice) {
-			this.HidDevice = hidDevice;
-		}
-		public override string ToString() {
-			return Name;
-		}
+        public event EventHandler StateUpdated;
 
-		public static IEnumerable<MuniaController> ListDevices() {
-			var ldr = new HidDeviceLoader();
-			
-			// MUNIA devices with 0x1209 VID
-			foreach (var device in ldr.GetDevices(0x1209)) {
-				if (device.ProductName == "NinHID NGC") {
-					yield return new MuniaNgc(device);
-				}
-				else if (device.ProductName == "NinHID N64") {
-					yield return new MuniaN64(device);
-				}
-				else if (device.ProductName == "NinHID SNES") {
-					yield return new MuniaSnes(device);
-				}
-			}
+        public class UltraTime
+        {
+            public readonly TimeSpan TimeSpan;
+            public readonly DateTime DateTime;
+            public readonly AtomicDateTime AtomicDateTime;
+            public readonly TimeSpan Jitter;
+            public byte[] Inputs;
+            public UltraTime()
+            {
+                TimeSpan = LogWatch.Elapsed;
+                DateTime = DateTime.UtcNow;
+                AtomicDateTime = TimeStamp.CurrentDateTime;
+                Jitter = LogWatch.Elapsed - TimeSpan;
+            }
+        }
 
-			// MUSIA devices
-			foreach (var device in ldr.GetDevices(0x1209, 0x8844)) {
-				if (device.ProductName == "MUSIA PS2 controller") {
-					yield return new MusiaPS2(device);
-				}
-			}
+        protected MuniaController(HidDevice hidDevice)
+        {
+            this.HidDevice = hidDevice;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static IEnumerable<MuniaController> ListDevices()
+        {
+            var ldr = new HidDeviceLoader();
+
+            // MUNIA devices with 0x1209 VID
+            foreach (var device in ldr.GetDevices(0x1209))
+            {
+                if (device.ProductName == "NinHID NGC")
+                {
+                    yield return new MuniaNgc(device);
+                }
+                else if (device.ProductName == "NinHID N64")
+                {
+                    yield return new MuniaN64(device);
+                }
+                else if (device.ProductName == "NinHID SNES")
+                {
+                    yield return new MuniaSnes(device);
+                }
+            }
+
+            // MUSIA devices
+            foreach (var device in ldr.GetDevices(0x1209, 0x8844))
+            {
+                if (device.ProductName == "MUSIA PS2 controller")
+                {
+                    yield return new MusiaPS2(device);
+                }
+            }
 
 
-			// legacy VID/PID combo from microchip
-			foreach (var device in ldr.GetDevices(0x04d8, 0x0058)) {
-				if (device.ProductName == "NinHID NGC" || device.GetMaxInputReportLength() == 9 && device.GetMaxOutputReportLength() == 0) {
-					yield return new MuniaNgc(device);
-				}
-				else if (device.ProductName == "NinHID N64" || device.GetMaxInputReportLength() == 5) {
-					yield return new MuniaN64(device);
-				}
-				else if (device.ProductName == "NinHID SNES" || device.GetMaxInputReportLength() == 3) {
-					yield return new MuniaSnes(device);
-				}
-			}
+            // legacy VID/PID combo from microchip
+            foreach (var device in ldr.GetDevices(0x04d8, 0x0058))
+            {
+                if (device.ProductName == "NinHID NGC" || device.GetMaxInputReportLength() == 9 && device.GetMaxOutputReportLength() == 0)
+                {
+                    yield return new MuniaNgc(device);
+                }
+                else if (device.ProductName == "NinHID N64" || device.GetMaxInputReportLength() == 5)
+                {
+                    yield return new MuniaN64(device);
+                }
+                else if (device.ProductName == "NinHID SNES" || device.GetMaxInputReportLength() == 3)
+                {
+                    yield return new MuniaSnes(device);
+                }
+            }
 
-		}
+        }
 
-		public static IEnumerable<ConfigInterface> GetConfigInterfaces() {
-			return GetMuniaConfigInterfaces().OfType<ConfigInterface>()
-				.Union(GetMusiaConfigInterfaces());
-		}
+        public static IEnumerable<ConfigInterface> GetConfigInterfaces()
+        {
+            return GetMuniaConfigInterfaces().OfType<ConfigInterface>()
+                .Union(GetMusiaConfigInterfaces());
+        }
 
-		public static IEnumerable<MuniaConfigInterface> GetMuniaConfigInterfaces() {
-			var ldr = new HidDeviceLoader();
-			var candidates = ldr.GetDevices(0x04d8, 0x0058)
-				.Union(ldr.GetDevices(0x1209));
-			foreach (HidDevice dev in candidates.Where(device => device.ProductName == "NinHID CFG"
-				|| device.GetMaxInputReportLength() == device.GetMaxOutputReportLength()
-					&& device.GetMaxOutputReportLength() == 9))
-				yield return new MuniaConfigInterface(dev);
-		}
+        public static IEnumerable<MuniaConfigInterface> GetMuniaConfigInterfaces()
+        {
+            var ldr = new HidDeviceLoader();
+            var candidates = ldr.GetDevices(0x04d8, 0x0058)
+                .Union(ldr.GetDevices(0x1209));
+            foreach (HidDevice dev in candidates.Where(device => device.ProductName == "NinHID CFG"
+                || device.GetMaxInputReportLength() == device.GetMaxOutputReportLength()
+                    && device.GetMaxOutputReportLength() == 9))
+                yield return new MuniaConfigInterface(dev);
+        }
 
-		public static IEnumerable<HidDevice> GetMuniaBootloaderInterfaces() {
-			var ldr = new HidDeviceLoader();
-			return ldr.GetDevices(0x04d8, 0x003c);
-		}
+        public static IEnumerable<HidDevice> GetMuniaBootloaderInterfaces()
+        {
+            var ldr = new HidDeviceLoader();
+            return ldr.GetDevices(0x04d8, 0x003c);
+        }
 
-		public static IEnumerable<MusiaConfigInterface> GetMusiaConfigInterfaces() {
-			var ldr = new HidDeviceLoader();
-			foreach (HidDevice dev in ldr.GetDevices(0x1209, 0x8844).Where(d => d.ProductName.Contains("config")))
-				yield return new MusiaConfigInterface(dev);
-		}
+        public static IEnumerable<MusiaConfigInterface> GetMusiaConfigInterfaces()
+        {
+            var ldr = new HidDeviceLoader();
+            foreach (HidDevice dev in ldr.GetDevices(0x1209, 0x8844).Where(d => d.ProductName.Contains("config")))
+                yield return new MusiaConfigInterface(dev);
+        }
 
-		public class StreamAndBuffer {
-			public byte[] buffer;
-			public Stream stream;
-		}
+        public class StreamAndBuffer
+        {
+            public byte[] buffer;
+            public Stream stream;
+        }
 
-		/// <summary>
-		/// Registers device for receiving inputs.
-		/// </summary>
-		public bool Activate() {
-			try {
-				_stream?.Dispose();
-				_stream = HidDevice.Open();
-				_stream.ReadTimeout = Timeout.Infinite;
+        /// <summary>
+        /// Registers device for receiving inputs.
+        /// </summary>
+        public bool Activate()
+        {
+            try
+            {
+                _stream?.Dispose();
+                _stream = HidDevice.Open();
+                _stream.ReadTimeout = Timeout.Infinite;
 
-				byte[] buffer = new byte[HidDevice.GetMaxInputReportLength()];
-				var sb = new StreamAndBuffer { buffer = buffer, stream = _stream };
+                byte[] buffer = new byte[HidDevice.GetMaxInputReportLength()];
+                var sb = new StreamAndBuffer { buffer = buffer, stream = _stream };
 
-				_stream.BeginRead(buffer, 0, buffer.Length, Callback, sb);
-				return true;
-			}
-			catch {
-				return false;
-			}
-		}
+                _stream.BeginRead(buffer, 0, buffer.Length, Callback, sb);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
-		public void Deactivate() {
-			_stream?.Dispose();
-			_stream = null;
-		}
-		public bool IsActive => _stream != null && _stream.CanRead;
-		public bool IsAvailable {
-			get {
-				HidStream s;
-				var ret = HidDevice.TryOpen(out s);
-				if (!ret) return false;
-				ret = s.CanRead;
-				s.Close();
-				return ret;
-			}
-		}
+        public void Deactivate()
+        {
+            _stream?.Dispose();
+            _stream = null;
+        }
+        public bool IsActive => _stream != null && _stream.CanRead;
+        public bool IsAvailable
+        {
+            get
+            {
+                HidStream s;
+                var ret = HidDevice.TryOpen(out s);
+                if (!ret) return false;
+                ret = s.CanRead;
+                s.Close();
+                return ret;
+            }
+        }
 
-		private void Callback(IAsyncResult ar) {
-			var sb = (StreamAndBuffer)ar.AsyncState;
-			try {
-				int numBytes = sb.stream.EndRead(ar);
-				if (numBytes > 0) {
-					if (Parse(sb.buffer))
-						StateUpdated?.Invoke(this, EventArgs.Empty);
-					sb.stream.BeginRead(sb.buffer, 0, sb.buffer.Length, Callback, sb);
-				}
-			}
-			catch (IOException exc) {
-				_stream = null;
-				Debug.WriteLine("IOException: " + exc.Message);
-				sb.stream.Dispose();
-			}
-			catch (ObjectDisposedException) { }
-			catch (NullReferenceException) { }
-		}
+        private void Callback(IAsyncResult ar)
+        {
+            var log = new UltraTime();
+            var sb = (StreamAndBuffer)ar.AsyncState;
+            try
+            {
+                int numBytes = sb.stream.EndRead(ar);
+                if (numBytes > 0)
+                {
+                    if (Parse(sb.buffer))
+                        StateUpdated?.Invoke(this, EventArgs.Empty);
+                    sb.stream.BeginRead(sb.buffer, 0, sb.buffer.Length, Callback, sb);
+                    log.Inputs = (byte[])sb.buffer.Clone();
+                    InputLog.Add(log);
 
-		public void Dispose() {
-			_stream?.Dispose();
-		}
+                    if (sb.buffer[1] == 4)
+                    {
+                        var s = string.Empty;
+                        var fuckyoumore = InputLog.ToArray();
+                        lock (fuckyoumore)
+                            foreach (var x in fuckyoumore)
+                            {
+                                s += x.TimeSpan.Ticks.ToString() + '\t' +
+                                    x.DateTime.Ticks.ToString() + '\t' +
+                                    x.AtomicDateTime.Time.Ticks.ToString() + '\t' +
+                                    x.AtomicDateTime.SyncedWithAtomicClock.ToString() + '\t' +
+                                    x.Jitter.Ticks.ToString() + '\t';
+                                foreach (var b in x.Inputs)
+                                {
+                                    s += b.ToString() + '\t';
+                                }
+                                s += "\r\n";
+                            }
+                        File.WriteAllText(@"C:\inputLog4.txt", s);
+                    }
 
-		protected abstract bool Parse(byte[] ev);
+                }
+                if (numBytes != 5)
+                    throw new Exception("test");
+            }
+            catch (IOException exc)
+            {
+                _stream = null;
+                Debug.WriteLine("IOException: " + exc.Message);
+                sb.stream.Dispose();
+            }
+            catch (ObjectDisposedException) { }
+            catch (NullReferenceException) { }
+        }
 
-	}
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+
+        protected abstract bool Parse(byte[] ev);
+
+    }
 }
---
+++ MUNIA-win/MUNIA/Util/NTP.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MUNIA.Util
+{
+    public class NTP
+    {
+        // stackoverflow.com/a/12150289
+        public static DateTime Now
+        {
+            get
+            {
+                const string ntpServer = "time.windows.com";
+
+                var ntpData = new byte[48];
+
+                ntpData[0] = 0x1B;
+
+                var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+                var ipEndPoint = new IPEndPoint(addresses[0], 123);
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                socket.Connect(ipEndPoint);
+
+                socket.ReceiveTimeout = 1000;
+
+                var before = TimeStamp.Now;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+
+                var delta = TimeSpan.FromTicks((TimeStamp.Now - before).Ticks / 2);
+
+                socket.Close();
+
+                const byte serverReplyTime = 40;
+
+                long intPart = SwapEndianness(BitConverter.ToUInt32(ntpData, serverReplyTime));
+                long fractPart = SwapEndianness(BitConverter.ToUInt32(ntpData, serverReplyTime + 4));
+
+                var ticks = (intPart * 10000000L) + ((fractPart * 10000000L) / 0x100000000L);
+
+                var networkDateTime = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddTicks(ticks);
+
+                return networkDateTime + delta;
+            }
+        }
+
+        // stackoverflow.com/a/3294698
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+    }
+}
